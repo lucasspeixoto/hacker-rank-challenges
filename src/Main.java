ab8c1ce8bf62e1886c4d15e18240cde887bc8c2d
Lucas Peixoto <lspeixotodev@gmail.com>
2023-06-28 21:37:31 -0300

    Mini max sum exercise

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import challenges.MiniMaxSum;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        MiniMaxSum.calculateMinMaxSum();
+
+    }
+
+}
---
+++ src/challenges/MiniMaxSum.java
@@ -0,0 +1,44 @@
+package challenges;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
+
+/**
+ * Exercise: https://www.hackerrank.com/challenges/mini-max-sum/problem
+ */
+
+public class MiniMaxSum {
+
+    public static void calculateMinMaxSum() throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<Integer> arr = new ArrayList<>(
+                Stream.of(
+                                bufferedReader.readLine()
+                                        .replaceAll("\\s+$", "")
+                                        .split(" "))
+                        .map(Integer::parseInt)
+                        .toList());
+
+
+        Collections.sort(arr);
+
+        int maxArrValue = arr.stream().reduce(arr.get(0), Integer::max);
+
+        int minArrValue = arr.stream().reduce(arr.get(0), Integer::min);
+
+        int minSum = arr.stream().reduce(0, Integer::sum) - maxArrValue;
+        int maxSum = arr.stream().reduce(0, Integer::sum) - minArrValue;
+
+        System.out.print(minSum);
+        System.out.print("  ");
+        System.out.print(maxSum);
+
+    }
+}
