6c206d37834eebf76609341795a2ea91e597d856
Lucas Peixoto <lspeixotodev@gmail.com>
2023-06-28 22:27:47 -0300

    Refactoring mini max sum exercise

--- src/challenges/MiniMaxSum.java
+++ src/challenges/MiniMaxSum.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -14,11 +13,11 @@ import java.util.stream.Stream;
 
 public class MiniMaxSum {
 
-    public static void calculateMinMaxSum() throws IOException {
+    public static void main(String[] args) throws IOException {
 
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-        List<Integer> arr = new ArrayList<>(
+        List<Integer> integerItems = new ArrayList<>(
                 Stream.of(
                                 bufferedReader.readLine()
                                         .replaceAll("\\s+$", "")
@@ -28,19 +27,40 @@ public class MiniMaxSum {
 
         bufferedReader.close();
 
+        int maxArrValue = 0;
+        int minArrValue = 0;
+        int arrTotalSum = 0;
 
-        Collections.sort(arr);
+        for (Integer integer : integerItems) {
+            arrTotalSum += integer;
+
+            if (integer > maxArrValue) {
+                maxArrValue = integer;
+            } else if (integer < minArrValue) {
+                minArrValue = integer;
+            }
+        }
+
+        System.out.print(arrTotalSum - maxArrValue);
+        System.out.print(" ");
+        System.out.print(arrTotalSum - minArrValue);
+
+        /*
 
         int maxArrValue = arr.stream().reduce(arr.get(0), Integer::max);
 
         int minArrValue = arr.stream().reduce(arr.get(0), Integer::min);
 
         int minSum = arr.stream().reduce(0, Integer::sum) - maxArrValue;
+
         int maxSum = arr.stream().reduce(0, Integer::sum) - minArrValue;
 
         System.out.print(minSum);
         System.out.print("  ");
         System.out.print(maxSum);
+
+
+         */
 
     }
 }
